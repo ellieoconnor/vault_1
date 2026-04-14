dbe580629977b427856ef1ae0d3aa94da79d5cd3
ellieoconnor <ellieoconnor3114@gmail.com>
2026-04-14 19:33:13 -0400

    Task 9 complete: wire new routes in App.tsx

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -4,6 +4,8 @@ import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
 import { AuthGuard } from "./components/shared/AuthGuard";
 import DashboardPage from "./pages/DashboardPage";
+import ForgotPasswordPage from "./pages/ForgotPasswordPage";
+import ResetPasswordPage from "./pages/ResetPasswordPage";
 
 export default function App() {
   return (
@@ -12,6 +14,8 @@ export default function App() {
         {/* Public routes */}
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+        <Route path="/reset-password/:token" element={<ResetPasswordPage />} />
 
         {/* Protected routes */}
         <Route
